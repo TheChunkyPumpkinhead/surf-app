ac9efb63e0b02abbd39393d92ce43c3f99e0575a
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-06-06 19:51:57 -0700

    Fixing issues with Database

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
 
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT ||
-    // "http://localhost:8080/api"
-     "https://surf-jotz.herokuapp.com/api"
+  API_ENDPOINT:
+    process.env.REACT_APP_API_ENDPOINT || "http://localhost:8080/api",
+  //  "https://surf-jotz.herokuapp.com/api"
 };
-
-
